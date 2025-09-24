47e12a2cae158f3283ac9e86a3a610135aed743f
chrisawoke <uchechristian27@gmail.com>
2025-09-24 15:36:20 +0100

    Update Day 2 - Practices on JOINS & Basic Window Function.sql

--- Week 1/Day 2 - Practices on JOINS & Basic Window Function.sql
+++ Week 1/Day 2 - Practices on JOINS & Basic Window Function.sql
@@ -2,8 +2,41 @@
 
 /*
 Goal:
-- Strengthen SQL joins with multiple dimensions (AdventureWorks OLAP)
+- Strengthen SQL joins with multiple dimensions
 - Understand window functions deeply
 - Practice ranking, running totals, comparisons
 - Keep theory + practice together for review
 */
+
+-- Task 1: List each internet order with customer full name, product name, and territory name.
+SELECT
+	f.SalesOrderNumber,
+	CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+	p.EnglishProductName AS ProductName,
+	s.SalesTerritoryRegion AS TerritoryName,
+	f.SalesAmount
+FROM FactInternetSales f
+INNER JOIN DimCustomer c
+	ON f.CustomerKey = c.CustomerKey
+INNER JOIN DimProduct p
+	ON f.ProductKey = p.ProductKey
+INNER JOIN DimSalesTerritory s
+	ON f.SalesTerritoryKey = s.SalesTerritoryKey;
+
+-- Task 2: Show total sales per customer by territory
+SELECT
+	CONCAT(c.FirstName, ' ', c.LastName) AS Customer,
+	s.SalesTerritoryRegion AS Territory,
+	SUM(f.SalesAmount) AS TotalSales
+FROM FactInternetSales f
+INNER JOIN DimCustomer c
+	ON f.CustomerKey = c.CustomerKey
+INNER JOIN DimSalesTerritory s
+	ON f.SalesTerritoryKey = s.SalesTerritoryKey
+GROUP BY
+	CONCAT(c.FirstName, ' ', c.LastName),
+	s.SalesTerritoryRegion
+ORDER BY
+	TotalSales DESC;
+
+
