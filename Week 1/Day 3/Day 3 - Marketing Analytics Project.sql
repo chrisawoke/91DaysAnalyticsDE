9122510afe45b522caf7d4942abaa333c4d638ab
chrisawoke <uchechristian27@gmail.com>
2025-09-25 13:28:35 +0100

    Day 3

---
+++ Week 1/Day 3/Day 3 - Marketing Analytics Project.sql
@@ -0,0 +1,25 @@
+-- Write a query to categorize the products based on price
+
+SELECT 
+	ProductID,
+	ProductName,
+	Price,
+	CASE
+		WHEN Price < 50 THEN 'Low'
+		WHEN Price BETWEEN 50 AND 200 THEN 'Medium'
+		ELSE 'High'
+	END AS PriceCategory
+FROM dbo.products
+ORDER BY Price DESC
+
+-- Write a query to join customer and geography
+SELECT
+	c.CustomerName,
+    c.Email,
+    c.Gender,
+    c.Age,
+    g.Country,
+    g.City
+FROM dbo.customers c
+LEFT JOIN dbo.geography g
+ON c.GeographyID = g.GeographyID
