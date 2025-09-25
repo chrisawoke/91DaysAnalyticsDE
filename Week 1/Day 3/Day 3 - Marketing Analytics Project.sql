812dd6b346abfee03cd0f55f1ab68b73a8aec02a
chrisawoke <uchechristian27@gmail.com>
2025-09-25 13:47:35 +0100

    Update Day 3 - Marketing Analytics Project.sql

--- Week 1/Day 3/Day 3 - Marketing Analytics Project.sql
+++ Week 1/Day 3/Day 3 - Marketing Analytics Project.sql
@@ -1,8 +1,26 @@
 -- Key Task: clean the dataset and get it ready for analysis
+
 /*Do the following:
-1. Write a query to categorize the products based on price
-2. 
+1. Write a query to join customer and geography tables (it's good for the Dmodel in BI)
+2. Write a query to categorize the products based on price
+3. Clean & standardize the engagement data table
+4. Remove whitespaces from the customer_reviews table (ReviewText column)
+5. 
 */
+
+-- Task 1: 
+SELECT
+	c.CustomerName,
+    c.Email,
+    c.Gender,
+    c.Age,
+    g.Country,
+    g.City
+FROM dbo.customers c
+LEFT JOIN dbo.geography g
+ON c.GeographyID = g.GeographyID
+
+-- Task 2:
 SELECT 
 	ProductID,
 	ProductName,
@@ -15,14 +33,3 @@ SELECT
 FROM dbo.products
 ORDER BY Price DESC
 
--- Write a query to join customer and geography
-SELECT
-	c.CustomerName,
-    c.Email,
-    c.Gender,
-    c.Age,
-    g.Country,
-    g.City
-FROM dbo.customers c
-LEFT JOIN dbo.geography g
-ON c.GeographyID = g.GeographyID
